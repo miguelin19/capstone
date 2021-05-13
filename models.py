01e2c2d6f92398436e53e23150c8e5c329d8c110
miguelin19 <miguel_carra19@hotmail.com>
2021-05-13 20:44:25 +0200

    reset models.py

--- models.py
+++ models.py
@@ -56,7 +56,7 @@ class Actor(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(120),nullable=False)
     age = db.Column(db.Integer, nullable=False)
-    gender = db.Column(ENUM('female', 'male', name='gender_enum', create_type=False), nullable=False)
+    gender = db.Column(db.String(120), nullable=False)
 
     def insert(self):
         db.session.add(self)
