108cb32df9403c6b02a0bfcf2cad16a2e19f257e
miguelin19 <miguel_carra19@hotmail.com>
2021-05-13 19:28:29 +0200

    fix enum

--- models.py
+++ models.py
@@ -57,7 +57,7 @@ class Actor(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(120),nullable=False)
     age = db.Column(db.Integer, nullable=False)
-    gender = db.Column(db.ENUM('female', 'male', name='gender_enum', create_type=False), nullable=False)
+    gender = db.Column(ENUM('female', 'male', name='gender_enum', create_type=False), nullable=False)
     movies = db.relationship('Casting', back_populates='_actor', cascade="all, delete")
 
     def insert(self):
