422e378035a411805f57c322c1ff8842cef5a77c
miguelin19 <miguel_carra19@hotmail.com>
2021-05-13 17:24:38 +0200

    added setup_db to create_app()

--- app.py
+++ app.py
@@ -3,13 +3,14 @@ from flask import Flask, request, abort, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_cors import CORS
-from models import Actor, Movie
+from models import Actor, Movie, setup_db
 from auth import AuthError, requires_auth
 
 def create_app(test_config=None):
   # create and configure the app
   app = Flask(__name__)
   CORS(app)
+  setup_db(app)
 
   # 'GET' endpoints
   @app.route('/movies')
